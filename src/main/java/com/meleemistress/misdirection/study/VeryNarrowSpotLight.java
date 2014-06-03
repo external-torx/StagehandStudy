75aefdc677a77e61ca32fd565e39e1ab4c559d87
Hallie Parry <halloran.parry@gmail.com>
2014-06-03 11:03:46 -0700

    Initial composite pattern implementation

---
+++ src/main/java/com/meleemistress/misdirection/study/VeryNarrowSpotLight.java
@@ -0,0 +1,32 @@
+/** 
+ * (C) Copyright 2014 Halloran Parry. All Rights Reserved
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.meleemistress.misdirection.study;
+
+/**
+ * @author hparry
+ *
+ */
+public class VeryNarrowSpotLight implements Lamp {
+
+    /* (non-Javadoc)
+     * @see com.meleemistress.misdirection.study.Lamp#illuminate()
+     */
+    public void illuminate() {
+        System.out.println("Don't point that thing in my eyes...Don't point that thing in my--DON'T POINT THAT THING IN MY EYES!");
+        
+    }
+
+}
